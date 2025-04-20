e8f15745e6478b6cd1f13d887a8fefcb47db3ed5
jlPortus <jl_puerto@hotmail.com>
2025-04-20 18:09:07 +0200

    Componente de core estructura

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -2,30 +2,22 @@ import { Component } from '@angular/core';
 import { GridModule, PagerModule } from '@syncfusion/ej2-angular-grids';
 
 import { RouterOutlet } from '@angular/router';
-
-import { PageSettingsModel } from '@syncfusion/ej2-angular-grids';
-import { data } from './datasource';
+import { HeaderComponent } from "./core/estructura/header/header.component";
+import { BodyComponent } from "./core/estructura/body/body.component";
+import { FooterComponent } from "./core/estructura/footer/footer.component";
 
 @Component({
   selector: 'app-root',
   imports: [
-    GridModule,
-    PagerModule,
-    RouterOutlet
-  ],
+    HeaderComponent,
+    BodyComponent,
+    FooterComponent
+],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'frontend';
+  title = 'Rodrigo';
 
-    public data?: object[];
-  public pageSettings?: PageSettingsModel;
-  
-  ngOnInit(): void {
-    this.data = data;
-
-    // The pageSettings property is also set to specify the page size for the Grid
-    this.pageSettings = { pageSize: 10 };
-  }
+   
 }
