13ac28d196e20ca7a5f5ac4ddf18f6f330080cc2
jlPortus <jl_puerto@hotmail.com>
2025-04-20 20:56:28 +0200

    un menu de navegacion guarro

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,21 +1,91 @@
 import { Component } from '@angular/core';
+import {
+  ButtonModule,
+  CheckBoxModule,
+  RadioButtonModule,
+  SwitchModule,
+  ChipListModule,
+  FabModule,
+  SpeedDialModule,
+  SmartPasteButtonModule,
+} from '@syncfusion/ej2-angular-buttons';
+import {
+  DropDownListModule,
+  ComboBoxModule,
+  AutoCompleteModule,
+  MultiSelectModule,
+  ListBoxModule,
+  DropDownTreeModule,
+  MentionModule,
+} from '@syncfusion/ej2-angular-dropdowns';
+import { ListViewModule } from '@syncfusion/ej2-angular-lists';
+import {
+  DropDownButtonModule,
+  SplitButtonModule,
+  ProgressButtonModule,
+} from '@syncfusion/ej2-angular-splitbuttons';
+import {
+  AccordionModule,
+  ToolbarModule,
+  ContextMenuModule,
+  BreadcrumbModule,
+  CarouselModule,
+  TabModule,
+  TreeViewModule,
+  SidebarModule,
+  MenuModule,
+  AppBarModule,
+  StepperModule,
+} from '@syncfusion/ej2-angular-navigations';
 
-import { HeaderComponent } from "./core/estructura/header/header.component";
-import { BodyComponent } from "./core/estructura/body/body.component";
-import { FooterComponent } from "./core/estructura/footer/footer.component";
+import { HeaderComponent } from './core/estructura/header/header.component';
+import { BodyComponent } from './core/estructura/body/body.component';
+import { FooterComponent } from './core/estructura/footer/footer.component';
+import { RouterOutlet, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   imports: [
+    ButtonModule,
+    CheckBoxModule,
+    RadioButtonModule,
+    SwitchModule,
+    ChipListModule,
+    FabModule,
+    SpeedDialModule,
+    SmartPasteButtonModule,
+    DropDownListModule,
+    ComboBoxModule,
+    AutoCompleteModule,
+    MultiSelectModule,
+    ListBoxModule,
+    DropDownTreeModule,
+    MentionModule,
+    ListViewModule,
+    DropDownButtonModule,
+    SplitButtonModule,
+    ProgressButtonModule,
+    AccordionModule,
+    ToolbarModule,
+    ContextMenuModule,
+    BreadcrumbModule,
+    CarouselModule,
+    TabModule,
+    TreeViewModule,
+    SidebarModule,
+    MenuModule,
+    AppBarModule,
+    StepperModule,
     HeaderComponent,
     BodyComponent,
-    FooterComponent
-],
+    FooterComponent,
+    RouterOutlet,
+    RouterModule,
+    
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'Rodrigo';
-
-   
 }
--- frontend/src/app/core/estructura/header/header.component.ts
+++ frontend/src/app/core/estructura/header/header.component.ts
@@ -1,11 +1,101 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation, Inject, ViewChild } from '@angular/core';
+import {
+  SidebarComponent,
+  SidebarModule,
+  AccordionModule,
+  ExpandEventArgs,
+  Accordion,
+  AccordionClickArgs,
+  AccordionComponent,
+  ClickEventArgs,
+} from '@syncfusion/ej2-angular-navigations';
+import { Router, RouterModule } from '@angular/router';
+import { ListViewModule } from '@syncfusion/ej2-angular-lists';
+import { from } from 'rxjs';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [SidebarModule, AccordionModule, RouterModule],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  encapsulation: ViewEncapsulation.None,
+  styleUrl: './header.component.css',
 })
 export class HeaderComponent {
+  constructor(private _router: Router) {
 
+     // Vincular el método al contexto de la clase para que este método pueda acceder a las propiedades de la clase
+    this.clicked = this.clicked.bind(this);
+  }
+
+  @ViewChild('sidebar')
+  public sidebarObj?: SidebarComponent;
+  @ViewChild('accordion')
+  public accordionObj?: AccordionComponent;
+
+  public data: { [key: string]: Object }[] = [
+    {
+      header: 'Grid',
+      content: '<div id="Appliances_Items"></div>',
+      subItems: [
+        {
+          header: 'Kitchen',
+          content: '<div id="Appliances_Kitchen_Items"></div>',
+          subItems: [
+            {
+              header: 'home',
+            },
+            {
+              header: 'Grid',
+            },
+            { header: 'Blenders' },
+          ],
+        },
+      ],
+    },
+  ];
+
+  //Expanding Event function for Accordion component.
+  public expand(e: ExpandEventArgs): void {
+    if (e.isExpanded) {
+      if (
+        (e.element as HTMLElement)
+          .getElementsByClassName('e-acrdn-content')[0]
+          .children[0].classList.contains('e-accordion')
+      ) {
+        return;
+      }
+      //Initialize Nested Accordion component
+      let nestAcrdn: Accordion = new Accordion({
+        items: (<{ subItems: object[] }>e.item).subItems,
+        expanding: this.expand,
+        clicked: this.clicked,
+      });
+
+      let elemId: string = (e.element as HTMLElement).getElementsByClassName(
+        'e-acrdn-content'
+      )[0].children[0].id;
+      //Render initialized Nested Accordion component
+      nestAcrdn.appendTo('#' + elemId);
+    }
+  }
+
+  public clicked(e: AccordionClickArgs): void {
+    switch (e.item?.header?.toString()) {
+      case 'home':
+        this._router.navigate(['/']);
+        break;
+      case 'Grid':
+        this._router.navigate(['/grid']);
+        break;
+    }
+  }
+
+  public hamburgerClick(): void {
+    this.sidebarObj?.show();
+    (this.accordionObj as AccordionComponent).refresh();
+  }
+
+  public close(): void {
+    this.sidebarObj?.hide();
+  }
 }
