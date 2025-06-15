3c1b831692931ae30e2575bbac089a0ba8675cb4
jlPortus <jl_puerto@hotmail.com>
2025-06-15 18:35:32 +0200

    limpiando campos no usados

--- frontend/src/app/componentes/areas/areas.component.ts
+++ frontend/src/app/componentes/areas/areas.component.ts
@@ -24,24 +24,24 @@ areasService: AreasService;
   columns: ColumnConfig[] = [
     { field: 'nombre', headerText: 'Nombre', isPrimaryKey: true, width: 150 },
     //{ field: 'descripcion', headerText: 'Descripción', width: 250 },
-    { field: 'conceptos_interes_ids', headerText: 'Conceptos', width: 150 },
+    /* { field: 'conceptos_interes_ids', headerText: 'Conceptos', width: 150 },
     { field: 'fuentes_ids', headerText: 'Fuentes', width: 120, textAlign: 'Center' },
     // Tus campos estáticos adicionales…
     { field: 'num_menciones', headerText: 'Menciones', textAlign: 'Right' },
     { field: 'usuario_creador', headerText: 'Creador' },
     { field: 'fecha_creacion', headerText: 'Fecha Creación', type:'datetime', format:'yyyy-MM-dd' },
-    { field: 'activa', headerText: 'Activa', type: 'boolean', width: 80, textAlign: 'Center' }
+    { field: 'activa', headerText: 'Activa', type: 'boolean', width: 80, textAlign: 'Center' } */
   ];
 
   formFields: FormFieldConfig[] = [
     { name: 'nombre', label: 'Nombre', type: 'text', validators: [ Validators.required ] },
-    { name: 'conceptos_interes_ids', label: 'Conceptos', type: 'text', placeholder: 'sepáralas por comas' },
+    /* { name: 'conceptos_interes_ids', label: 'Conceptos', type: 'text', placeholder: 'sepáralas por comas' },
     {
       name: 'fuentes_ids',
       label: 'Fuentes',
       type: 'select',
       options: [{ value: 'twiter', label: 'Twitter'}, ]  // si quieres cargarlas dinámicamente
-    }
+    } */
   ];
  
 }
